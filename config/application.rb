ce31920e951fd588af6d6b5347b19de3c036e5af
Tom Doom <malaclypse17@web.de>
2013-02-24 10:11:26 +0100

    application.rb modified

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "sprockets/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -27,7 +31,7 @@ module TokLesesaal
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+       config.time_zone = 'Europe/Berlin'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -38,6 +42,8 @@ module TokLesesaal
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+	
+	config.assets.precompile += %w( bootstrap-responsive-custom.css )
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
